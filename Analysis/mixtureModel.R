9d00442053e256e8efa68fc7357ff3b608d6fdd0
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2018-10-22 12:27:15 +1100

    Functional model, figuring out whether it makes sense for the random effect of source to be limited to the observed effect, as opposed to impacting the true effects.

--- Analysis/mixtureModel.R
+++ Analysis/mixtureModel.R
@@ -1,41 +1,77 @@
 library(rjags)
+library(brms)
+
+# https://osf.io/xhj4d/  - Camerer, C. F., Dreber, A., Holzmeister, F., Ho, T.-H., Huber, J., Johannesson, M., . . . Wu, H. (2018). Evaluating the replicability of social science experiments in Nature and Science between 2010 and 2015. Nature Human Behaviour, 2(9), 637-644. doi:10.1038/s41562-018-0399-z
+# Getting rid of missing data
+jagData <- allData[ !is.na(allData$fis.o)& !is.na(allData$seFish.r) & !is.na(allData$seFish.o) & !is.na(allData$fis.r),] 
+
+nrow(jagData)
+## ADDING IN MISSING DATA 
+jagData <- allData[ !is.na(allData$fis.o) & !is.na(allData$n.o) & !is.na(allData$n.r) & !is.na(allData$fis.r),] 
+jagData$seFish.o <- sqrt(1/(jagData$n.o-3))
+jagData$seFish.r <- sqrt(1/(jagData$n.r-3))
+
+
+# Setting up the model 
+## NOTE: JAGS uses the precision, not the variance, when specifying a normal distribution (precision = 1/variance), i.e., 1/(se^2)
+jagMod <- jags.model(file = 'Analysis/BMWMod.R', 
+                     data = list(orgEffect_FT = jagData$fis.o, n = length(jagData$fis.o), repTau = 1/(jagData$seFish.r^2), orgTau = 1/(jagData$seFish.o^2), repEffect_FT = jagData$fis.r),
+                     n.chains=4)
+
+# Parameters to keep
+params <- c(# "clust",
+  #"orgEffect_FT" ,
+  #"repEffect_FT" ,
+  #"trueRepEffect",
+  "alpha")
+
+# Running model and summarising 
+samples <- coda.samples(jagMod,params,n.iter = 10000)
+samplesSum<-summary(samples)
+# Highest prob density interval
+HPDinterval(samples)
+traceplot(samples)
+# Traceplot - be warned, this takes forever unless you are only looking at one parameter at a time - I suggest alpha, the most important (assuming all else is working)
+# traceplot(samples)
+
+# Taking a burn in of 5000 out
+summary(window(samples, start=5001))
+
+# plot(samples)
+
+
+### additional model - it should also account for random effects for both study and for source
+
+jagMod_additional <- jags.model(file = 'Analysis/BMWMod_additional.R', 
+                     data = list(orgEffect_FT = jagData$fis.o, n = length(jagData$fis.o), repTau = 1/(jagData$seFish.r^2), orgTau = 1/(jagData$seFish.o^2), repEffect_FT = jagData$fis.r,
+                                 nSource = length(unique(jagData$source)), #nStudy = length(unique(jagData$authorsTitle.o)), 
+                                 #study = as.factor(as.character(jagData$authorsTitle.o)), 
+                                 source = as.factor(as.character(jagData$source))),
+                     n.chains=4)
+
+
+# Parameters to keep
+params <- c("mu_source_rep",
+  #"clust",
+  #"orgEffect_FT" ,
+  #"trueOrgEffect",
+  #"repEffect_FT" ,
+  #"trueRepEffect",
+  "alpha")
+
+# Running model and summarising 
+samples2 <- coda.samples(jagMod_additional,params,n.iter = 10000)
+samples2Sum<-summary(samples2)
+samples2Sum
+plot(samples2)
+View(samples2Sum)
+
+sums <- do.call(cbind.data.frame, samples2Sum)
+View(data.frame(sums))
 
 
 
-modelString <- 
-"
-model{
-# Mixture Model Priors:
-alpha ~ dunif(0,1) # flat prior on slope for predicted effect size under H1
-tau ~ dgamma(0.001,0.001) # vague prior on study precision
-phi ~ dbeta(1, 1) # flat prior on the true effect rate
-# prior on true effect size of original studies:
-for (i in 1:n){
-trueOrgEffect[i] ~ dnorm(0, 1)
-}
-# Mixture Model Likelihood:
-for(i in 1:n){
-clust[i] ~ dbern(phi)# extract errors in variables (FT stands for Fisher-transformed):
-orgEffect_FT[i] ~ dnorm(trueOrgEffect[i], orgTau[i])
-repEffect_FT[i] ~ dnorm(trueRepEffect[i], repTau[i])
-trueRepEffect[i] ~ dnorm(mu[i], tau)
-# if clust[i] = 0 then H0 is true; if clust[i] = 1 then H1 is true and
-# the replication effect is a function of the original effect:
-mu[i] <- alpha * trueOrgEffect[i] * equals(clust[i], 1)
-# when clust[i] = 0, then mu[i] = 0;
-# when clust[i] = 1, then mu[i] = alpha * trueOrgEffect[i]
-  }
-}
-"
-rjags::jags.model(file = 'Analysis/mixtureModel.R', 
-                  data = list(orgEffect_FT = allData$fis.o, repEffect_FT = allData$fis.r, seFish.o = allData$seFish.o,  seFish.r = allData$seFish.r))
-                  
-  
-  
-model.file = modelString, 
-dataList = list(study = allData$authorsTitle.o, fis.o = allData$fis.o, fis.r = allData$fis.r, seFish.o = allData$seFish.o,  seFish.r = allData$seFish.r)
-parameters = 
-n.chains = 4
-inits.fn = NULL
-n.ad
+
+
+
 
